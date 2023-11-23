1b122fd4b137c62b00708b780d76d881ad6ba9a0
Agustín Cejas <125676291+JustinGame414@users.noreply.github.com>
2023-11-23 01:40:23 -0300

    Add files via upload

---
+++ Resolvente Funciones.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+	int resolv (float a, float b, float c, float *x1, float *x2){
+	*x1 =(-b+sqrt(b*b-4*a*c))/(2*a);
+	*x2 =(-b-sqrt(b*b-4*a*c))/(2*a);
+	return 0;
+}
+int main(void){
+	float 	A,B,C,x1,x2;
+	printf("Ingrese los coeficientes A:\n");
+	scanf("%f",&A);
+	printf("Ingrese los coeficientes B\n");
+	scanf("%f",&B); 
+	printf("Ingrese los coeficientes C:\n");
+	scanf("%f",&C);
+	resolv(A,B,C,&x1,&x2);
+	printf("La raiz x1 es:%f\n",x1);
+	printf("La raiz x2 es:%f\n",x2);
+	return 0;
+}
---
+++ tp6 p7.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+void Raices(float a,float b,float c,float* raiz1,float* raiz2){
+	float discriminante=b*b-4*a*c;
+	*raiz1 = (-b+sqrt(discriminante))/(2*a);
+	*raiz2 = (-b-sqrt(discriminante))/(2*a);
+}
+int main() {
+	float a,b,c;
+	float raiz1, raiz2;
+	
+	printf("ingrese los coeficientes:");
+	scanf("%f %f %f",&a,&b,&c);
+	
+	Raices(a,b,c,&raiz1,&raiz2);
+	
+	printf("las raices son: %.2f y %.2f\n",raiz1,raiz2);
+	
+	return 0;
+}
